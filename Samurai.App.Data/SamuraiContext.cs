b8909b52ce4dbdd8ff732fca15072f5f69058acc
JackTorrence <jacktorrence420@gmx.com>
2017-11-08 23:17:17 +0000

    Add project files.

---
+++ Samurai.App.Data/SamuraiContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext: DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>()
+                .HasKey(s => new {s.BattleId, s.SamuraiId});
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // define the database to use
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+
+    }
+}
